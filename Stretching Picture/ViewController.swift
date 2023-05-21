301b2df823333dc33b5b4345ab2e6d71f563f80a
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-21 12:04:59 +0300

    Task completed

--- Stretching Picture/ViewController.swift
+++ Stretching Picture/ViewController.swift
@@ -8,13 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(named: "twitter")
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+//        imageView.frame.origin = .zero
+        imageView.frame.size = CGSize(width: view.frame.width, height: 400)
+        return imageView
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.insetsLayoutMarginsFromSafeArea = false
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        
+        scrollView.frame = view.frame
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 1500)
+        scrollView.verticalScrollIndicatorInsets.top = 400 - view.safeAreaInsets.top
+        
+        return scrollView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
     }
 
 
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0 {
+            let imageViewHeight = 400 - scrollView.contentOffset.y
+            
+            imageView.frame.origin = CGPoint(x: 0, y: scrollView.contentOffset.y)
+            imageView.frame.size = CGSize(width: view.frame.width, height: imageViewHeight)
+            scrollView.verticalScrollIndicatorInsets.top = imageViewHeight - view.safeAreaInsets.top
+        }
+    }
+}
