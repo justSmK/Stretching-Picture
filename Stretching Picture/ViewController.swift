2b980912a2958aa7c37f4dcaf7eaf4ad00c8ce44
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-22 15:28:38 +0300

    Best solution

--- Stretching Picture/ViewController.swift
+++ Stretching Picture/ViewController.swift
@@ -7,53 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
-    private let height: CGFloat = 270
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
-    private lazy var imageView: UIImageView = {
-        let imageView = UIImageView()
-        let image = UIImage(named: "twitter")
-        imageView.image = image
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.frame.origin = .zero
-        imageView.frame.size = CGSize(width: view.frame.width, height: height)
-        return imageView
-    }()
-    
-    private lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.insetsLayoutMarginsFromSafeArea = false
-        scrollView.contentInsetAdjustmentBehavior = .never
-        scrollView.delegate = self
-        
-        scrollView.frame = view.frame
-        scrollView.contentSize = CGSize(width: view.frame.width, height: 1500)
-        scrollView.verticalScrollIndicatorInsets.top = height - view.safeAreaInsets.top
-        
-        return scrollView
-    }()
+    let imageView = UIImageView(image: .init(named: "image"))
+    let scrollView = UIScrollView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        
         view.addSubview(scrollView)
+        scrollView.delegate = self
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
         scrollView.addSubview(imageView)
+        
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        imageView.frame = .init(x: .zero, y: -scrollView.safeAreaInsets.top, width: scrollView.frame.width, height: imageHeight)
+        
+        scrollView.contentSize = .init(width: view.frame.width, height: 2000)
+    }
+    
+    private let imageHeight: CGFloat = 270
 
-}
-
-extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.contentOffset.y < 0 {
-            let imageViewHeight = height - scrollView.contentOffset.y
-            
-            imageView.frame.origin = CGPoint(x: 0, y: scrollView.contentOffset.y)
-            imageView.frame.size = CGSize(width: view.frame.width, height: imageViewHeight)
-            scrollView.verticalScrollIndicatorInsets.top = imageViewHeight - view.safeAreaInsets.top
+        let y = scrollView.contentOffset.y + scrollView.safeAreaInsets.top
+        if y < 0 {
+            imageView.frame = .init(x: .zero, y: -scrollView.safeAreaInsets.top + y, width: scrollView.frame.width, height: imageHeight + abs(y))
         }
+        
+//        scrollView.scrollIndicatorInsets.top = imageView.frame.height - scrollView.safeAreaInsets.top
+        scrollView.verticalScrollIndicatorInsets.top = imageView.frame.height - scrollView.safeAreaInsets.top
     }
 }
